532db830aab5e03b19e57f370e677090539bea1f
dilushamadushan <dmdmadushan007@gmail.com>
2026-07-24 20:21:41 +0530

    feat: containerize backend and frontend services with Docker Compose and implement initial API client service

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use((req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running in ${PORT}`);
 })
